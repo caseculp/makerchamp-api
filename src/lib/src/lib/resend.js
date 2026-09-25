eee6bbe496b54ea15d45ad0f6f757ac5b712d62e
caseculp <54865299+caseculp@users.noreply.github.com>
2026-09-25 13:28:34 -0400

    Delete src/lib/src/lib directory

--- src/lib/src/lib/resend.js
+++
@@ -1,9 +0,0 @@
-const { Resend } = require('resend');
-
-if (!process.env.RESEND_API_KEY) {
-  console.warn('[makerchamp] RESEND_API_KEY is not set — merchant emails will fail.');
-}
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-module.exports = resend;
